5aa6dde6d8aafac5250e4601de8759770b9bebb6
Rio2904 <riosuryafernando12@gmail.com>
2025-08-17 12:12:13 +0700

    add dropdown button  certificate

--- src/Main/Home/Skill/Certificates.jsx
+++ src/Main/Home/Skill/Certificates.jsx
@@ -1,17 +1,67 @@
-export default function Certificates(){
+import { useState } from "react"
+import { ThemeContext } from "../../../Context/ThemeContext"
+import { useContext } from "react"
+
+export default function Certificates() {
+  const [show, setShow] = useState(false);
+
+  function handleClick() {
+    setShow((prev) => !prev);
+  }
+
+  return (
+    <div className="skill-container">
+      <div className="skill-section">
+        <div className="skill-title">
+          <h4>Certificate</h4>
+        </div>
+        <div className="sertif-items">
+          <div className="sertif-title">Javascript Fundamental</div>
+          <div className="sertif-platform">Dicoding Indonesia</div>
+          <div className="sertif-date">July 31, 2023</div>
+          <a className="sertif-link" href="lihat">View Certificate</a>
+        </div>
+
+        {show && <More/>}
+
+        {!show && <Button title="View more" onClick={handleClick} show={show}/>}
+        {show && <Button title="View less" onClick={handleClick} show={show}/>}
+      </div>
+    </div>
+  );
+}
+
+function Button({ title, onClick, show }) {
+  const { theme } = useContext(ThemeContext);
+  const svgColor = theme === 'light' ? "#000000" : "white";
+  
+  let svg = <svg width="80px" height="80px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z" fill={svgColor} /></svg>
+  if (show) svg = <svg width="80px" height="80px" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M903.232 768l56.768-50.432L512 256l-448 461.568 56.768 50.432L512 364.928z" fill={svgColor} /></svg>
+  
+  return (
+    <span id="certifBtn" onClick={onClick}>
+        {title} 
+        {svg}
+    </span>
+  );
+}
+
+
+function More(){
     return (
-        <div className="skill-container">
-            <div className="skill-section">
-                <div className="skill-title">
-                    <h4>Certificate</h4>
-                </div>
-                <div className="sertif-items">
+        <>  
+            <div className="sertif-items">
                     <div className="sertif-title">Javascript Fundamental</div>
                     <div className="sertif-platform">Dicoding Indonesia</div>
                     <div className="sertif-date">July 31, 2023</div>
                     <a className="sertif-link" href="lihat">View Certificate</a>
                 </div>
-            </div>
-        </div>
+            <div className="sertif-items">
+                    <div className="sertif-title">Javascript Fundamental</div>
+                    <div className="sertif-platform">Dicoding Indonesia</div>
+                    <div className="sertif-date">July 31, 2023</div>
+                    <a className="sertif-link" href="lihat">View Certificate</a>
+                </div>
+        </>
     )
 }
