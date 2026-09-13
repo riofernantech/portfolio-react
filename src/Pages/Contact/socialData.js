6fd7fc82c8ea6cd2ef4711946a16a258b9acae27
Rio2904 <riosuryafernando12@gmail.com>
2026-09-13 10:48:00 +0700

    feat: add project images and update project data; refactor social icons and intro links

--- src/Pages/Contact/socialData.js
+++ src/Pages/Contact/socialData.js
@@ -10,7 +10,7 @@ import {
 export const socialItems = [
     { label: 'Facebook', href: 'https://www.facebook.com/profile.php?id=100018635116359&mibextid=ZbWKwL', Icon: FacebookIcon },
     { label: 'Whatsapp', href: 'https://wa.me/6289520316261', Icon: WhatsAppIcon },
-    { label: 'Twitter', href: 'https://twitter.com/RioFernando_29', Icon: TwitterIcon },
+    { label: 'X', href: 'https://twitter.com/RioFernando_29', Icon: TwitterIcon },
     { label: 'Instagram', href: 'https://www.instagram.com/rio.fernand0/', Icon: InstagramIcon },
     { label: 'Telegram', href: 'https://t.me/rio_fernando_s', Icon: TelegramIcon },
     { label: 'Discord', href: 'http://discordapp.com/users/607433056906903592', Icon: DiscordIcon },
--- src/Pages/Project/projectData.js
+++ src/Pages/Project/projectData.js
@@ -7,14 +7,43 @@ import {
     BootstrapIcon,
     NodeIcon,
     ElectronIcon,
+    CodeIgniterIcon,
+    SqlIcon,
 } from '../Home/Skill/icons/SkillIcons';
 
 export const projectItems = [
     {
+        id: 'sisawit',
+        title: 'SI Sawit',
+        description: 'Sistem informasi untuk pencatatan transaksi penimbangan buah sawit pada kelompok tani.',
+        image: '/images/projects/sisawit.png',
+        source: 'https://github.com/riofernantech/si-sawit',
+        techStack: [
+            { label: 'JavaScript', Icon: JavaScriptIcon },
+            { label: 'React JS', Icon: ReactIcon },
+            { label: 'Node JS', Icon: NodeIcon },
+            { label: 'Electron JS', Icon: ElectronIcon },
+            { label: 'Sqlite', Icon: SqlIcon },
+        ],
+    },
+    {
+        id: 'rf-blog',
+        title: 'RF Blog',
+        description: 'CMS multi-user untuk pembuatan dan pengelolaan blog pribadi atau tim.',
+        image: '/images/projects/rfblog.png',
+        visit: 'https://blog.riofernando.my.id',
+        source: 'https://github.com/riofernantech/rf-blog',
+        techStack: [
+            { label: 'PHP', Icon: PhpIcon },
+            { label: 'JavaScript', Icon: JavaScriptIcon },
+            { label: 'CSS', Icon: CssIcon },
+        ],
+    },
+    {
         id: 'rf-music',
         title: 'RF Music',
         description: 'Aplikasi pemutar musik berbasis web yang mengonsumsi data dari API eksternal.',
-        image: '/images/rfmusic.png',
+        image: '/images/projects/rfmusic.png',
         visit: 'https://music.riofernando.my.id',
         source: 'https://github.com/riofernantech/rf-music',
         techStack: [
@@ -27,38 +56,14 @@ export const projectItems = [
         id: 'kaori',
         title: 'Landing Page LPK-Kaori',
         description: 'Website landing page dan CMS untuk organisasi lembaga pelatihan Kaori.',
-        image: '/images/kaori.png',
+        image: '/images/projects/kaori.png',
         source: 'https://github.com/riofernantech/lpk-kaori',
         techStack: [
-            { label: 'PHP', Icon: PhpIcon },
+            { label: 'CodeIgniter', Icon: CodeIgniterIcon },
             { label: 'JavaScript', Icon: JavaScriptIcon },
-            { label: 'CSS', Icon: CssIcon },
+            { label: 'Bootstrap', Icon: BootstrapIcon },
         ],
     },
-    {
-        id: 'sisawit',
-        title: 'SI Sawit',
-        description: 'Sistem informasi untuk pencatatan transaksi penimbangan buah sawit pada kelompok tani.',
-        image: '/images/sisawit.png',
-        source: 'https://github.com/riofernantech/si-sawit',
-        techStack: [
-            { label: 'JavaScript', Icon: JavaScriptIcon },
-            { label: 'React JS', Icon: ReactIcon },
-            { label: 'Node JS', Icon: NodeIcon },
-            { label: 'Electron JS', Icon: ElectronIcon },
-        ],
-    },
-    {
-        id: 'rf-blog',
-        title: 'RF Blog',
-        description: 'CMS multi-user untuk pembuatan dan pengelolaan blog pribadi atau tim.',
-        image: '/images/rfblog.png',
-        visit: 'https://blog.riofernando.my.id',
-        source: 'https://github.com/riofernantech/rf-blog',
-        techStack: [
-            { label: 'PHP', Icon: PhpIcon },
-            { label: 'JavaScript', Icon: JavaScriptIcon },
-            { label: 'CSS', Icon: CssIcon },
-        ],
-    },
+
+
 ];
